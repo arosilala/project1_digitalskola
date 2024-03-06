bfa3c1142a86346445b172b24071ee5615140293
arosilala <161470506+arosilala@users.noreply.github.com>
2024-03-06 11:58:54 +0700

    Create library_items.py

---
+++ modules/library_items.py
@@ -0,0 +1,7 @@
+class LibraryItem:
+    def __init__(self, title: str = None, upc: str = None, subject: str = None, volume: str = None, issue: str = None):
+        self.title = title
+        self.upc = upc
+        self.subject = subject
+        self.volume = volume
+        self.issue = issue
